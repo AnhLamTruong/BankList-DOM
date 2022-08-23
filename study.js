4ce1939b6c8faa4a974d089e9a34f3104eb79b50
STSC <STSC@LENOVO-127800>
2022-08-23 15:20:07 -0700

    Finishing js DOM manipulation

--- study.js
+++ study.js
@@ -1,16 +1,17 @@
-'use strict';
+"use strict";
 ////////////////////////////////////////////////////////////////////////
 // Selecting elements
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
 // console.log(allSections);
-const allButtons = document.getElementsByTagName('button');
+const allButtons = document.getElementsByTagName("button");
 // console.log(allButtons);
 
 // Creating and inserting elements
 // .insertAdjacentHTML
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.classList.add("smooth-fade");
 message.innerHTML =
   'We use cookied for improve functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 // header.prepend(message);
@@ -19,28 +20,28 @@ header.append(message);
 ////////////////////////////////////////////////////////////////////////////////////
 //Delete elements
 document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
     message.remove();
   });
 
 //////////////////////////////////////////////////////////////////////////////////
 //Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
 
 // console.log(getComputedStyle(message).color);
 // console.log(getComputedStyle(message).height);
 // console.log(message.style.backgroundColor);
 
 message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
 
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// document.documentElement.style.setProperty("--color-primary", "orangered");
 
 /////////////////////////////////////////////////////////////////////////////////////
 //Atributes
-const logo = document.querySelector('.nav__logo');
+const logo = document.querySelector(".nav__logo");
 // console.log(logo.alt);
 // console.log(logo.src);
 
@@ -51,10 +52,10 @@ const logo = document.querySelector('.nav__logo');
 // console.log(logo.dataset.versionNumber);
 
 //Classes
-logo.classList.add('c');
-logo.classList.remove('c');
-logo.classList.toggle('c');
-logo.classList.contains('c');
+logo.classList.add("c");
+logo.classList.remove("c");
+logo.classList.toggle("c");
+logo.classList.contains("c");
 
 // const h1 = document.querySelector('h1');
 
@@ -122,4 +123,56 @@ logo.classList.contains('c');
 //   if (el !== h1) {
 //     el.style.background = 'var(--gradient-primary)';
 //   }
+// });
+
+//Sticky note
+// const initialCoords = section1.getBoundingClientRect();
+// // console.log(initialCoords);
+
+// window.addEventListener("scroll", function (e) {
+//   // console.log(window.scrollY);
+//   if (window.scrollY > initialCoords.top) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// });
+
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
+//     if (!ent.isIntersecting) {
+//       document.nav.classList.add("sticky");
+//     }
+//     if (ent.isIntersecting) {
+//       document.nav.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
+// obs.observe(head);
+// const obsCallBack = function (entries, ob) {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: 0.2,
+// };
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  console.log("HTML parsed and DOM tree built", e);
+});
+
+// window.addEventListener('beforeunload', function(e){
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue='';
 // });
