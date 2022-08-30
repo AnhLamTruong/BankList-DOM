18847467462c1c9277c7ba01f3b7cf58b6d8d511
STSC <STSC@LENOVO-127800>
2022-08-29 22:40:15 -0700

    studying

--- study.js
+++ study.js
@@ -167,12 +167,60 @@ logo.classList.contains("c");
 //   threshold: 0.2,
 // };
 
-document.addEventListener("DOMContentLoaded", function (e) {
-  console.log("HTML parsed and DOM tree built", e);
-});
-
 // window.addEventListener('beforeunload', function(e){
 //   e.preventDefault();
 //   console.log(e);
 //   e.returnValue='';
 // });
+
+// class expression
+// const PersonCl=class{
+
+// }
+
+//class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+  set fullName(name) {
+    if (name.include(" ")) {
+      this.fullName = name;
+    } else {
+      alert(`${name} is not a full name!`);
+    }
+  }
+}
+
+const jessica = new PersonCl("Jessica Davis", 1996);
+console.log(jessica);
+console.log(jessica.age);
+
+// // PersonCl.prototype.greet = function () {
+// //   console.log(`Hey ${this.firstName}`);
+// // };
+
+// jessica.greet();
+
+// const account = {
+//   owner: "jonas",
+//   movements: [200, 530, 120, 300],
+
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
+
+// console.log(account.latest);
+// account.latest = 50;
+// console.log(account);
